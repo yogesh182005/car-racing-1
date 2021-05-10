74cc2409725c83782cc434b8fe88516954f5eb7c
yogesh <arjunsingh35273@gmail.com>
2021-05-10 16:01:46 +0530

    changed canvas,added sprites,camera

--- sketch.js
+++ sketch.js
@@ -5,12 +5,14 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
+var car1,car2,car3,car4;
+var cars;
 
 var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
